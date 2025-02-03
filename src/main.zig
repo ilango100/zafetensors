d05947b97064e0d40107837d7d5263cffbf684c9
Ilango Rajagopal <ilangokul@gmail.com>
2025-02-03 21:30:41 +0530

    Commandline-utility "show", "convert" commands

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,67 @@
 const std = @import("std");
 const SafeTensors = @import("SafeTensors.zig");
-const convert = @import("convert.zig");
+const conversion = @import("conversion.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const file_path = "model.safetensors";
-    const file = try std.fs.cwd().openFile(file_path, .{ .mode = .read_only });
-    var st = try SafeTensors.openFile(allocator, file);
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+    _ = args.next().?;
+    var exit_code = 0;
+    if (args.next()) |arg| {
+        if (std.mem.eql(u8, arg, "show")) {
+            exit_code = show(allocator, &args);
+        } else if (std.mem.eql(u8, arg, "convert")) {
+            exit_code = try convert(allocator, &args);
+        }
+    }
+    std.process.exit(exit_code);
+}
+
+const show_usage =
+    \\ Usage: show <file>
+    \\  <file>: safetensors file path
+;
+
+pub fn show(allocator: std.mem.Allocator, args: *std.process.ArgIterator) u8 {
+    const path = args.next() orelse {
+        std.debug.print("{s}\n", .{show_usage});
+        return 1;
+    };
+    const st = SafeTensors.open(allocator, path) catch |e| {
+        std.debug.print("Unable to open safetensors file: {}", .{e});
+        return 1;
+    };
+    defer st.deinit();
+    var it = st.header.iterator();
+    while (it.next()) |entry| {
+        std.debug.print("{s} {} {any}\n", .{ entry.key_ptr.*, entry.value_ptr.dtype, entry.value_ptr.shape });
+    }
+    return 0;
+}
+
+const convert_usage =
+    \\ Usage: convert <file> <dtype>
+    \\  <file>: safetensors file path
+    \\  <dtype>: Dtype to convert to
+;
+
+pub fn convert(allocator: std.mem.Allocator, args: *std.process.ArgIterator) !u8 {
+    const path = args.next() orelse {
+        std.debug.print("{s}\n", .{convert_usage});
+        return 1;
+    };
+    const dtype = args.next() orelse {
+        std.debug.print("{s}\n", .{convert_usage});
+        return 1;
+    };
+
+    std.debug.print("convert {s} {s}\n", .{ path, dtype });
+
+    var st = try SafeTensors.open(allocator, path);
     defer st.deinit();
 
     var it = st.header.iterator();
@@ -20,7 +72,7 @@ pub fn main() !void {
         defer allocator.free(buf);
         switch (entry.value_ptr.dtype) {
             .BF16 => {
-                const f16_buf = try convert.bf16ToFP32(allocator, buf);
+                const f16_buf = try conversion.bf16ToFP32(allocator, buf);
                 defer allocator.free(f16_buf);
                 for (f16_buf[0..16]) |value| {
                     std.debug.print("{e:.4} ", .{value});
