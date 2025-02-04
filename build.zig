1436b05a4c94b4ee92a5ef1cf07623c067068e06
Ilango Rajagopal <ilangokul@gmail.com>
2025-02-05 01:47:31 +0530

    fix: missed changing in build script

--- build.zig
+++ build.zig
@@ -23,7 +23,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const compile_test = b.addTest(.{
-        .root_source_file = b.path("src/ZafeTensors.zig"),
+        .root_source_file = b.path("src/SafeTensors.zig"),
         .target = target,
         .optimize = optimize,
     });
